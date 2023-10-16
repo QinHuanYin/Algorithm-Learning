12a48bbd2c5bf320cd6d6ae68a58c6120a7ec01c
wuzixianhao <496984587@qq.com>
2023-10-16 22:10:30 +0800

    Add Pair.java

---
+++ src/learning1/Pair.java
@@ -0,0 +1,16 @@
+package learning1;
+
+public class Pair {
+    private int key;
+    private String val;
+
+    public Pair() {
+
+    }
+
+    private Pair(int key, String val) {
+        this.key = key;
+        this.val = val;
+    }
+
+}
